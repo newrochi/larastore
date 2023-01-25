1b8765df5c4c087525c77d04c1a29fd19354a54a
newrochi <yusufyuzlu2008@gmail.com>
2023-01-25 03:33:44 -0400

    created getters and setters for Product attributes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,13 +7,6 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /* public static $products=[
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "image" => "game.png", "price"=>"1000"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "image" => "safe.png", "price"=>"999"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "image" => "submarine.png", "price"=>"30"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "image" => "game.png", "price"=>"100"]
-    ]; */
-
     public function index(){
         $viewData=[];
         $viewData['title']='Products - Online Store';
@@ -25,8 +18,8 @@ class ProductController extends Controller
     public function show($id){
         $viewData=[];
         $product=Product::findOrFail($id);
-        $viewData['title']=$product['name']." - Online Store";
-        $viewData['subtitle']=$product['name']." - Product Information";
+        $viewData['title']=$product->getName()." - Online Store";
+        $viewData['subtitle']=$product->getName()." - Product Information";
         $viewData['product']=$product;
 
         return view('product.show')->with('viewData',$viewData);
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,85 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+
+    /**
+    * PRODUCT ATTRIBUTES
+    * $this->attributes['id'] - int - contains the product primary key (id)
+    * $this->attributes['name'] - string - contains the product name
+    * $this->attributes['description'] - string - contains the product description
+    * $this->attributes['image'] - string - contains the product image
+    * $this->attributes['price'] - int - contains the product price
+    * $this->attributes['created_at'] - timestamp - contains the product creation date
+    * $this->attributes['updated_at'] - timestamp - contains the product update date
+    */
+
+    //Id getter & setter
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+    public function setId($id)
+    {
+    $this->attributes['id'] = $id;
+    }
+    //name getter & setter
+    public function getName()
+    {
+    return $this->attributes['name'];
+    }
+    public function setName($name)
+    {
+    $this->attributes['name'] = $name;
+    }
+    //description getter & setter
+    public function getDescription()
+    {
+    return $this->attributes['description'];
+    }
+    public function setDescription($description)
+    {
+    $this->attributes['description'] = $description;
+    }
+    //image getter & setter
+    public function getImage()
+    {
+    return $this->attributes['image'];
+    }
+    public function setImage($image)
+    {
+    $this->attributes['image'] = $image;
+    }
+    //price getter & setter
+    public function getPrice()
+    {
+    return $this->attributes['price'];
+    }
+    public function setPrice($price)
+    {
+    $this->attributes['price'] = $price;
+    }
+    //created_at getter & setter
+    public function getCreatedAt()
+    {
+    return $this->attributes['created_at'];
+    }
+    public function setCreatedAt($createdAt)
+    {
+    $this->attributes['created_at'] = $createdAt;
+    }
+    //updated_at getter & setter
+    public function getUpdatedAt()
+    {
+    return $this->attributes['updated_at'];
+    }
+    public function setUpdatedAt($updatedAt)
+    {
+    $this->attributes['updated_at'] = $updatedAt;
+    }
 }
