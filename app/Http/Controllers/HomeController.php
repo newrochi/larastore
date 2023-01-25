c2a62ccf8102d20ade9298e53044dac79cce39bc
newrochi <yusufyuzlu2008@gmail.com>
2023-01-25 00:04:19 -0400

    refactored about and home views, routes and controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,10 +13,11 @@ class HomeController extends Controller
     }
 
     public function about(){
-        $title='About us - Online Store';
-        $subtitle='About us';
-        $description='This is an about page ...';
-        $author='Developed by: Pooria Samarbafzadeh';
-        return view('home.about',compact(['title','subtitle','description','author']));
+        $viewData=[];
+        $viewData['title']='About us - Online Store';
+        $viewData['subtitle']='About us';
+        $viewData['description']='This is an about page ...';
+        $viewData['author']='Developed by: Pooria Samarbafzadeh';
+        return view('home.about')->with('viewData',$viewData);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-
-});
+Route::get('/', [HomeController::class,'index'])->name('home.index');
 
 Route::get('/about',[HomeController::class,'about'])->name('home.about');
