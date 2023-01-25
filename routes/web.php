60cdd2ac1d0312403d7ccfe5ea65fed667ab3859
newrochi <yusufyuzlu2008@gmail.com>
2023-01-25 04:30:02 -0400

    created products list page in admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -23,5 +24,6 @@ Route::get('/products', [ProductController::class,'index'])->name('product.index
 Route::get('/products/{id}', [ProductController::class,'show'])->name('product.show');
 
 Route::get('/admin', [AdminHomeController::class,'index'])->name('admin.home.index');
+Route::get('/admin/products', [AdminProductController::class,'index'])->name('admin.product.index');
 
 
