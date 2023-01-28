5426878a4bcc68097541177ec1b4d23431b6e9c3
newrochi <yusufyuzlu2008@gmail.com>
2023-01-28 01:30:14 -0400

    implemented adding images to products in product form

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,4 +24,6 @@ class ProductController extends Controller
 
         return view('product.show')->with('viewData',$viewData);
     }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,6 @@ Route::get('/products/{id}', [ProductController::class,'show'])->name('product.s
 
 Route::get('/admin', [AdminHomeController::class,'index'])->name('admin.home.index');
 Route::get('/admin/products', [AdminProductController::class,'index'])->name('admin.product.index');
+Route::post('/admin/products/store', [AdminProductController::class,'store'])->name('admin.product.store');
 
 
