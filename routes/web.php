0ae167b4e9043a672682ccb73ae0c6d4151eb579
newrochi <yusufyuzlu2008@gmail.com>
2023-01-28 21:31:42 -0400

    added auth middleware

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,16 @@ Route::get('/about', [HomeController::class,'about'])->name('home.about');
 Route::get('/products', [ProductController::class,'index'])->name('product.index');
 Route::get('/products/{id}', [ProductController::class,'show'])->name('product.show');
 
-Route::get('/admin', [AdminHomeController::class,'index'])->name('admin.home.index');
-Route::get('/admin/products', [AdminProductController::class,'index'])->name('admin.product.index');
-Route::post('/admin/products/store', [AdminProductController::class,'store'])->name('admin.product.store');
-Route::delete('/admin/products/{id}/delete', [AdminProductController::class,'delete'])->name('admin.product.delete');
-Route::get('/admin/products/{id}/edit', [AdminProductController::class,'edit'])->name('admin.product.edit');
-Route::put('/admin/products/{id}/update', [AdminProductController::class,'update'])->name('admin.product.update');
+
+Route::middleware('admin')->group(function(){
+    Route::get('/admin', [AdminHomeController::class,'index'])->name('admin.home.index');
+    Route::get('/admin/products', [AdminProductController::class,'index'])->name('admin.product.index');
+    Route::post('/admin/products/store', [AdminProductController::class,'store'])->name('admin.product.store');
+    Route::delete('/admin/products/{id}/delete', [AdminProductController::class,'delete'])->name('admin.product.delete');
+    Route::get('/admin/products/{id}/edit', [AdminProductController::class,'edit'])->name('admin.product.edit');
+    Route::put('/admin/products/{id}/update', [AdminProductController::class,'update'])->name('admin.product.update');
+});
+
 
 
 
